0eb88d9ea4a1d757e959a0a83530d2786f2d2737
Eug1n1 <vkzheny@gmail.com>
2021-10-09 18:44:50 +0300

    derzhi

---
+++ lab_10/10-03.js
@@ -0,0 +1,69 @@
+const WebSocket = require('ws');
+const http = require('http')
+const url = require('url')
+const fs = require('fs')
+
+const wsServer = new WebSocket.Server({port: 1000});
+
+wsServer.broadcast = function (message)
+{
+    wsServer.clients.forEach(client =>
+    {
+        client.send(message)
+    })
+}
+
+wsServer.on('connection', (wsClient) =>
+{
+
+    console.log(wsClient._socket?.remoteAddress)
+
+    let num = 0
+    let i = 0
+
+    let reg = new RegExp(/^10-\d{2}-client:\s*(\d+)$/)
+
+    let interval = setInterval(() =>
+    {
+        wsClient.send(`10-01-server: ${num}->${i++}`)
+    }, 5000)
+
+    wsClient.on('message', function(message) {
+        process.stdout.write(message + '\n')
+
+        let matches = message.toString().match(reg)
+        wsServer.broadcast(`fake broadcast message after: ${message}`)
+        num = matches[1]
+    })
+
+    wsClient.on('close', () =>
+    {
+        clearInterval(interval)
+    })
+})
+
+http.createServer(requestsHandler).listen(3000)
+
+
+function requestsHandler(req, res)
+{
+    let path = './wsStart.html'
+
+    if (req.method === 'GET')
+    {
+
+        fs.stat(path, (err, stats) =>
+        {
+
+            let file = fs.readFileSync(path)
+
+            res.writeHead(200, {'Content-Type': 'text/html', 'Content-Length': stats.size})
+            res.end(file)
+        })
+
+        return
+    }
+
+    res.writeHead(400)
+    res.end('/start')
+}
---
+++ lab_9/09-08.js
@@ -0,0 +1,51 @@
+const http = require('http')
+const multiparty = require("multiparty");
+
+const options = {
+    hostname: 'localhost',
+    port: 3000,
+    method: 'GET'
+}
+
+const req = http.request(options, res => {
+
+    console.log(res.statusMessage)
+    console.log(res.statusCode)
+
+    let form  = new multiparty.Form({uploadDir: './static'})
+    let result = ''
+
+    form.on('field', (name, value) =>
+    {
+        console.log(name, value)
+        result += `${name} = ${value}\n`
+    })
+
+    form.on('file', (name, file) =>
+    {
+        console.log(name, file)
+        result += `${name} = ${file.originalFilename} : ${file.path}`
+    })
+
+    form.on('error', (err) =>
+    {
+        console.log(err)
+    })
+
+    form.on('close', () =>
+    {
+        console.log(result)
+    })
+
+    form.parse(res)
+
+    res.on('data', d => {
+        process.stdout.write(d)
+    })
+})
+
+req.on('error', error => {
+    console.error(error)
+})
+
+req.end()
