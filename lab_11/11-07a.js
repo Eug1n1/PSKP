0b66fab79357c2eee527cfa6a2afc0f3093b6c6d
Eug1n1 <vkzheny@gmail.com>
2021-10-14 12:33:28 +0300

    derzhi

---
+++ lab_11/11-07a.js
@@ -0,0 +1,31 @@
+const Client = require('rpc-websockets').Client
+const readline = require('readline');
+
+const client = new Client('ws://localhost:4000/')
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: `~>`
+});
+
+client.on('open', () =>
+{
+    rl.prompt();
+    rl.on('line', (line) =>
+    {
+        let args = line.split(' ')
+        switch (args[0]) {
+            case 'A':
+                client.notify('A')
+                break;
+            case 'B':
+                client.notify('B')
+                break;
+            case 'C':
+                client.notify('C')
+                break;
+        }
+        rl.prompt();
+    })
+})
+
