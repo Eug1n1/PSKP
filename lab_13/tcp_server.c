fc35dd719a2ba03ce9081fede985489bd617f343
Eugene <vkzheny@gmail.com>
2022-11-26 10:28:38 +0300

    delete trash comments

--- lab_13/tcp_server.c
+++ lab_13/tcp_server.c
@@ -17,12 +17,11 @@ int main(int argc, char const* argv[])
     char* prefix = "ECHO: ";
     char* echo_message = malloc(sizeof(char) * 1024);
 
-    // Creating socket file descriptor
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
-    // Forcefully attaching socket to the port 8080
+
     if (setsockopt(server_fd, SOL_SOCKET,SO_REUSEADDR | SO_REUSEPORT, &opt,sizeof(opt))) {
         perror("setsockopt");
         exit(EXIT_FAILURE);
@@ -30,7 +29,7 @@ int main(int argc, char const* argv[])
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
-    // Forcefully attaching socket to the port 8080
+
     if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
         perror("bind failed");
         exit(EXIT_FAILURE);
@@ -44,6 +43,8 @@ int main(int argc, char const* argv[])
         exit(EXIT_FAILURE);
     }
 
+    printf("connected\n");
+
     while ((valread = recv(new_socket, buffer, 1024, 0))) {
         printf("RECV: %s\n", buffer);
 
@@ -56,9 +57,8 @@ int main(int argc, char const* argv[])
 
         // free(echo_message);
     }
-    // closing the connected socket
+
     close(new_socket);
-    // closing the listening socket
     shutdown(server_fd, SHUT_RDWR);
     return 0;
 }
