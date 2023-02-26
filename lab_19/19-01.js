d5823424b8d7e320099a5915037410a0e1ae31cf
Eugene <vkzheny@gmail.com>
2023-02-26 16:59:50 +0300

    add deleteRouter

--- lab_19/19-01.js
+++ lab_19/19-01.js
@@ -8,9 +8,11 @@ const app = express()
 
 const getRouter = require('./getRouter')
 const postRouter = require('./postRouter')
+const deleteRouter = require('./deleteRouter')
 
 app.use('/', getRouter)
 app.use('/', postRouter)
+app.use('/', deleteRouter)
 
 
 app.listen(process.env.PORT)
---
+++ lab_19/deleteRouter.js
@@ -0,0 +1,95 @@
+const router = require('express').Router()
+const PrismaClient = require('@prisma/client').PrismaClient
+
+const prisma = new PrismaClient()
+
+router.delete('/api/faculties/:faculty', async (req, res) => {
+    try {
+        res.json(
+            await prisma.faculty.delete({
+                where: {
+                    faculty: req.params.faculty,
+                },
+            })
+        )
+    } catch (e) {
+        res.status(400)
+        res.json(e)
+    }
+})
+
+router.delete('/api/pulpits/:pulpit', async (req, res) => {
+    try {
+        res.json(
+            await prisma.pulpit.delete({
+                where: {
+                    pulpit: req.params.pulpit,
+                },
+            })
+        )
+    } catch (e) {
+        res.status(400)
+        res.json(e)
+    }
+})
+
+router.delete('/api/subjects/:subject', async (req, res) => {
+    try {
+        res.json(
+            await prisma.subject.delete({
+                where: {
+                    subject: req.params.subject,
+                },
+            })
+        )
+    } catch (e) {
+        res.status(400)
+        res.json(e)
+    }
+})
+router.delete('/api/teachers/:teacher', async (req, res) => {
+    try {
+        res.json(
+            await prisma.teacher.delete({
+                where: {
+                    teacher: req.params.teacher,
+                },
+            })
+        )
+    } catch (e) {
+        res.status(400)
+        res.json(e)
+    }
+})
+
+router.delete('/api/auditoriums/:auditorium', async (req, res) => {
+    try {
+        res.json(
+            await prisma.auditorium.delete({
+                where: {
+                    auditorium: req.params.auditorium,
+                },
+            })
+        )
+    } catch (e) {
+        res.status(400)
+        res.json(e)
+    }
+})
+
+router.delete('/api/auditoriumtypes/:auditoriumType', async (req, res) => {
+    try {
+        res.json(
+            await prisma.auditoriumType.delete({
+                where: {
+                    auditoriumType: req.params.auditoriumType,
+                },
+            })
+        )
+    } catch (e) {
+        res.status(400)
+        res.json(e)
+    }
+})
+
+module.exports = router
