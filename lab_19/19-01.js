44b735b78887b227cb31493038edf28abf3212a4
Eugene <vkzheny@gmail.com>
2023-02-26 17:00:20 +0300

    add putRouter

--- lab_19/19-01.js
+++ lab_19/19-01.js
@@ -8,10 +8,13 @@ const app = express()
 
 const getRouter = require('./getRouter')
 const postRouter = require('./postRouter')
+const putRouter = require('./putRouter')
 const deleteRouter = require('./deleteRouter')
 
+app.use(express.json())
 app.use('/', getRouter)
 app.use('/', postRouter)
+app.use('/', putRouter)
 app.use('/', deleteRouter)
 
 
---
+++ lab_19/putRouter.js
@@ -0,0 +1,102 @@
+const router = require('express').Router()
+const PrismaClient = require('@prisma/client').PrismaClient
+
+const prisma = new PrismaClient()
+
+router.put('/api/faculties', async (req, res) => {
+    try {
+        res.json(
+            await prisma.faculty.update({
+                data: req.body,
+                where: {
+                    faculty: req.body.faculty,
+                },
+            })
+        )
+    } catch (e) {
+        res.status(400)
+        res.json(e)
+    }
+})
+
+router.put('/api/pulpits', async (req, res) => {
+    try {
+        res.json(
+            await prisma.pulpit.update({
+                data: req.body,
+                where: {
+                    pulpit: req.body.pulpit,
+                },
+            })
+        )
+    } catch (e) {
+        res.status(400)
+        res.json(e)
+    }
+})
+
+router.put('/api/subjects', async (req, res) => {
+    try {
+        res.json(
+            await prisma.subject.update({
+                data: req.body,
+                where: {
+                    subject: req.body.subject,
+                },
+            })
+        )
+    } catch (e) {
+        res.status(400)
+        res.json(e)
+    }
+})
+
+router.put('/api/teachers', async (req, res) => {
+    try {
+        res.json(
+            await prisma.teacher.update({
+                data: req.body,
+                where: {
+                    teacher: req.body.teacher,
+                },
+            })
+        )
+    } catch (e) {
+        res.status(400)
+        res.json(e)
+    }
+})
+
+router.put('/api/auditoriums', async (req, res) => {
+    try {
+        res.json(
+            await prisma.auditorium.update({
+                data: req.body,
+                where: {
+                    auditorium: req.body.auditorium,
+                },
+            })
+        )
+    } catch (e) {
+        res.status(400)
+        res.json(e)
+    }
+})
+
+router.put('/api/auditoriumtypes', async (req, res) => {
+    try {
+        res.json(
+            await prisma.auditoriumType.update({
+                data: req.body,
+                where: {
+                    auditoriumType: req.body.auditoriumType,
+                },
+            })
+        )
+    } catch (e) {
+        res.status(400)
+        res.json(e)
+    }
+})
+
+module.exports = router
