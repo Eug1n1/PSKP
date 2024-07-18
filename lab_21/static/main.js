59d6860b5ecc859cdc45abdb45e43ef952497b9e
Eug1n1 <vkzheny@gmail.com>
2024-07-18 13:12:55 +0300

    i

--- lab_21/static/main.js
+++ lab_21/static/main.js
@@ -1,11 +1,4 @@
 window.addEventListener('load', () => {
-    if (window.location.href.match(/\/add|update/)) {
-        const inputs = document.querySelectorAll('.contact_btn')
-        inputs.forEach((e) => {
-            e.disabled = true
-        })
-    }
-
     const delete_btn = document.querySelector('#delete_btn')
 
     document.querySelector('#name').addEventListener('input', () => {
